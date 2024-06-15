dccb41342739ddf4673e8bc41deacac36ef83533
SEO <sdm0875@naver.com>
2024-06-15 15:24:46 +0900

    24/06/15

--- CSS연습2/index.js
+++ CSS연습2/index.js
@@ -1,5 +1,7 @@
 const toggle__menu = document.querySelector(".toggle__menu");
+const toogle__sideBar = document.querySelector(".toogle__sideBar")
 
 function toggleSidebar() {
   toggle__menu.classList.toggle("active");
+  toogle__sideBar.classList.toggle("active");
 }
